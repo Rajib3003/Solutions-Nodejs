8e82a270123b6d7bdaac4e7f52fd2558ace50d15
Rajib3003 <115511670+Rajib3003@users.noreply.github.com>
2025-06-25 17:40:29 +0600

    error

--- module14/dist/app/app.js
+++ module14/dist/app/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,7 +17,42 @@ const todos_routes_1 = require("./todos/todos.routes");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use('/todos', todos_routes_1.todosRoute);
-app.get('/', (req, res) => {
-    res.send('wellcome to my app start');
+app.get('/', (req, res, next) => {
+    console.log({
+        url: req.url,
+        method: req.method,
+        headers: req.headers,
+    });
+    next();
+}, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        console.log(helloworld);
+        res.send('wellcome to my app start');
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+app.get('/error', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        console.log(helloworld);
+        res.send('wellcome to error er duniya');
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Route not found'
+    });
+});
+app.use((error, req, res, next) => {
+    if (error) {
+        console.log("error", error);
+        res.status(400).json({
+            message: 'Internal Server Error', error
+        });
+    }
 });
 exports.default = app;
--- module14/src/app/app.ts
+++ module14/src/app/app.ts
@@ -1,5 +1,5 @@
 
-import express, { Application, Request, Response } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 import fs from 'fs'
 import path from 'path'
 import { todosRoute } from './todos/todos.routes'
@@ -11,8 +11,49 @@ app.use(express.json())
 app.use('/todos', todosRoute );
 
 
-app.get('/', (req : Request, res : Response) => {
-  res.send('wellcome to my app start')
+app.get('/',
+  
+  (req : Request, res : Response, next: NextFunction) => {
+  
+    console.log({
+      url: req.url,
+      method: req.method,
+      headers: req.headers,
+   } )
+  next();
+}, 
+
+async (req : Request, res : Response, next: NextFunction) => {
+  try {
+    console.log(helloworld);
+    res.send('wellcome to my app start')
+  } catch (error) {
+    next(error);
+  }
+})
+
+app.get('/error',
+
+async (req : Request, res : Response, next: NextFunction) => {
+  try {
+     console.log(helloworld);
+    res.send('wellcome to error er duniya')
+  } catch (error) {
+    next(error);
+  }
+})
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    message: 'Route not found'
+  })});
+
+app.use((error:any,req: Request , res: Response,next: NextFunction)=>{
+  if(error){
+    console.log("error", error);
+    res.status(400).json({
+      message: 'Internal Server Error', error})
+  }
 })
 
 
